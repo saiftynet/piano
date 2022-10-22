1804435da8257bae9b0fecef1fe6cf5df2882e32
Saif Ahmed <34284663+saiftynet@users.noreply.github.com>
2022-10-22 10:21:32 +0100

    Update Attempt 2.pl

--- Attempt 2.pl
+++ Attempt 2.pl
@@ -1,6 +1,7 @@
 use strict; use warnings;
 use Term::ReadKey;
 use Time::HiRes ("sleep");      # allow fractional sleeps
+
 print "perl piano...sort of... please wait... intialising...\n";
 open (my $dsp,"|padsp tee /dev/audio > /dev/null") or   die qq(Couldn't execute for piping);
 my @octaves=("A","A#","B","C","C#","D","D#","E","F","F#","G","G#") x 8; # create 96 key piano
@@ -10,11 +11,14 @@ my $notes={};                   # hashref to store the notes with keys
 for my $k(1..@keys){
 	my $f=440*2**(($k-49)/12);              # generate frequency table
 	my $s=pack'C*',map 65*(1+sin($_*2*3.1415*($f/$sps))),0..$sps-1; # generate the sample
-	$notes->{$keys[$k-1]}={f=>$f,s=>$s};
+	$notes->{$keys[$k-1]}={f=>$f,s=>$s};    # store in hash
 	playNote($k-1);
 }
+
 sleep 10;
+
 print "Start pressing keys... Escape to exit;";
+
 my $refreshRate=50;    # read 50 times a second
 ReadMode 'cbreak';
 while(1){
